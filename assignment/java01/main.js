d9a792105dd92215c39b70475f4807af790cfba6
Errolzhu <xzhu7@saic.edu>
2020-10-26 17:57:11 -0500

    almost almost

--- assignment/java01/main.js
+++ assignment/java01/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
-    $("box").on("mousedown",function(){
-        $("box").html("<p>&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ≡つ<br>( つ 　≡つ＝つ<br>&nbsp;/ __ 　) パパパパ <br>&nbsp;&nbsp;( ／ Ｕ</p>")
+    $('html').on("click",function(){
+        $("#nekopunch").html("&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ≡つ<br>( つ 　≡つ＝つ<br>&nbsp;/ __ 　) パパパパ <br>&nbsp;&nbsp;( ／ Ｕ");
 
-        console.log("clicked")
+        console.log("clicked");
     });
 
+    $('html').off("click",function(){
+        $("#nekopunch").html("&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ<br>( つ 　≡つ<br>&nbsp;/ __ 　)<br>&nbsp;&nbsp;( ／ Ｕ");
 
+        console.log("back");
+    });
 })
 
 
