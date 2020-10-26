f18938196237c74339eba2f9c94bab3838e70574
Errolzhu <xzhu7@saic.edu>
2020-10-26 17:37:23 -0500

    almost done

--- assignment/java01/main.js
+++ assignment/java01/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
-    
+    $("box").on("mousedown",function(){
+        $("box").html("<p>&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ≡つ<br>( つ 　≡つ＝つ<br>&nbsp;/ __ 　) パパパパ <br>&nbsp;&nbsp;( ／ Ｕ</p>")
+
+        console.log("clicked")
+    });
 
 
 })
