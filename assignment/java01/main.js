02a82e8b906508dec50481571c4b7e1d95ce88b6
Errolzhu <xzhu7@saic.edu>
2020-10-26 20:43:42 -0500

    punching working

--- assignment/java01/main.js
+++ assignment/java01/main.js
@@ -1,16 +1,31 @@
 $(document).ready(function(){
 
-    $('html').one("click",function(){
-        $("#nekopunch").html("&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ≡つ<br>( つ 　≡つ＝つ<br>&nbsp;/ __ 　) パパパパ <br>&nbsp;&nbsp;( ／ Ｕ");
+    let noneClicked = true;
 
-        console.log("clicked");
+    $('html').mousedown(function(){
+        noneClicked = false;
+        swapPunch();
     });
 
-    $('html').off("click",function(){
-        $("#nekopunch").html("&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ<br>( つ 　≡つ<br>&nbsp;/ __ 　)<br>&nbsp;&nbsp;( ／ Ｕ");
-
-        console.log("back");
+    $('html').mouseup(function(){
+        noneClicked = true;
+        swapPunch();
+        
     });
+
+
+    function swapPunch(){
+        if(noneClicked==true){
+            $("#nekopunch").html("&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ<br>( つ 　≡つ<br>&nbsp;/ __ 　)<br>&nbsp;&nbsp;( ／ Ｕ");
+        }
+        else{
+            $("#nekopunch").html("&nbsp;&ensp;Λ_Λ<br>&nbsp;(　 ·ω·) ＝つ≡つ<br>( つ 　≡つ＝つ<br>&nbsp;/ __ 　) パパパパ <br>&nbsp;&nbsp;( ／ Ｕ");
+        }
+    }
 })
+
+
+
+
 
 
